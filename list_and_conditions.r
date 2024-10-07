83e211da3cb5f1e5961148e038dd69b609db3d5c
Ishmam Bin Rofi <ishmam.bin.rofi1@g.bracu.ac.bd>
2024-10-08 02:17:14 +0600

    first commit

---
+++ list_and_conditions.r
@@ -0,0 +1,13 @@
+number_1 <- 5
+number_2 <- 12
+
+mod <- number_2%% number_1
+print(mod)
+
+# print("Hello fucking world")
+a_vector <- c(1, 1, 1, 1, -1, -1)
+names(a_vector) <- c("CSE421", "CSE472", "CSE221", "CSE490", "CSE220", "CSE230")
+print(a_vector)
+
+boolean <- a_vector > 0
+print(boolean)
